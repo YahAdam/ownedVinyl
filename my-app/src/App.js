9d4360676c917636fdfbdf335609837a09bab98d
YahAdam <adamdmitchell98@gmail.com>
2023-01-08 16:17:48 -0500

    new divs, style updates

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,17 +5,24 @@ import { Component } from 'react';
 class App extends Component {
   state = {
     records: [{artist: 'Pearl Jam', title: 'Ten', color: 'Black'}, {artist: 'I Prevail', title: 'IDR', color: 'Black'}],
+    wishList: [{artist: 'Bring Me The Horizon', title: 'That\'s The Spirit'}],
   };
 
   render() {
     return (
       <div className="App">
+         <div className='wish__list'>
+            Wish list goes here
+          </div>
         <div className='App__container'>
         <div className='title'>
         Adam's owned vinyl
         </div>
         <Record records={this.state.records} />
         </div>
+        <div className='top__artists'>
+            Top artists goes here
+          </div>
       </div>
     );
   }
